e8fe2a2ffd8a5b3bc3f46e2b94fd60ee4ed08006
PRERAN01 <preran866@gmail.com>
2025-01-30 19:28:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let userinput;
+let max=100;
+let min=1;
+let randomnum=Math.round(Math.random()*(max-min))+min;
+let promt=window.prompt(`enter the number between ${min}-${max}`);
+let togle=true;
+while(togle)
+{
+    promt=Number(promt);
+    if(promt===randomnum)
+    {
+        window.alert("ha ha ha you won brother");
+        togle=false;
+    }
+    else if(promt<min || promt>max )
+    {
+        window.alert("ha ha ha enter valid number noob");
+        promt=window.prompt(`enter the number between ${min}-${max}`);
+    }
+    else if(isNaN(promt))
+    {
+        window.alert("enter a vaild number");
+        promt=window.prompt(`enter the number between ${min}-${max}`);
+    }
+    else{
+        if(promt>randomnum)
+            {
+                window.alert("ha ha ha enter a bit less ");
+                promt=window.prompt(`enter the number between ${min}-${max}`);
+            }
+            else if(promt<randomnum)
+            {
+                window.alert("ha ha ha enter a bit high");
+                promt=window.prompt(`enter the number between ${min}-${max}`);
+            }
+    }
+}
+
